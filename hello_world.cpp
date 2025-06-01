af17ce6b868806b61da9b3547feed6e29645987f
golubitskaiamariia <114326302+golubitskaiamariia@users.noreply.github.com>
2025-06-01 15:17:51 +0000

    Update comments hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <string>
 int main() {
-    std::string name;  // Create variable
-    std::cout << "Enter your name: ";  // Request value variable
-    std::cin >> name; // Get value variable
+    std::string name;  // Создаем переменную имени пользователя
+    std::cout << "Enter your name: ";  // Запрашиваем имя пользователя
+    std::cin >> name; // Получаем имя пользователя
     std::cout << "Hello world from " << name << std::endl;
     return 0;
 }
