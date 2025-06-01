6880ddc73d4474824f32a25a555545f8b9baba2e
golubitskaiamariia <mary.g.200617@gmail.com>
2025-06-01 12:35:13 +0000

    Add comments hello_world

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <string>
 int main() {
-    std::string name;
-    std::cout << "Enter your name: ";
-    std::cin >> name;
+    std::string name;  // Create variable
+    std::cout << "Enter your name: ";  // Request value variable
+    std::cin >> name; // Get value variable
     std::cout << "Hello world from " << name << std::endl;
     return 0;
 }
